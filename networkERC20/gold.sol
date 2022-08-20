221e58fc7719e8257effee738b7b4b70394240be
cyberfab007 <fabiano6@yahoo.com>
2022-08-20 00:43:39 -0400

    tokenupdates

--- networkERC20/gold.sol
+++ networkERC20/gold.sol
@@ -23,7 +23,7 @@ contract TokenERC20 {
     // Public variables of the token
     string public name;
     string public symbol;
-    uint8 public decimals = 2;
+    uint8 public decimals = 7;
     // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
 
