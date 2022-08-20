17f5713c2ba756e9834f9da0aab6297ee992f44b
cyberfab007 <fabiano6@yahoo.com>
2022-08-20 01:08:47 -0400

    contracts added

---
+++ ichorVerseGenerator/galaxys.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: CC - NON COMMERCIAL
+
+pragma solidity >=0.4.0 <0.6.0;
+
+contract galaxy{
+
+
+struct galaxy{
+int256 id;
+string name;
+
+
+}
+
+
+
+}
---
+++ ichorVerseGenerator/starsystem.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: CC - NON COMMERCIAL
+
+pragma solidity >=0.4.0 <0.6.0;
+
+contract stars{
+
+
+
+    
+}
